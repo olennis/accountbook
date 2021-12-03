4fcdcd7fa8eefc06de83e1bcb0b5ab59bd0e0652
olennis <kisses217@gmail.com>
2021-12-03 21:58:24 +0900

    💡 20211203 sideproject accountbook 슬랙 연결

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import { createEventAdapter } from '@slack/events-api';
+import { createServer } from 'http';
+import { WebClient } from '@slack/web-api'
+
+// 생성한 슬랙봇에 대한 키값들
+import CONFIG from '../config/bot.json';
+
+
+// 슬랙에서 슬랙봇에게 접근가능한 엔드포인트를 만들기 위해 웹서버(express)를 사용
+const app = express();
+
+const slackEvents = createEventAdapter(CONFIG.SIGNING_SECRET);
+const webClient = new WebClient(CONFIG.BOT_USER_OAUTH_ACCESS_TOKEN);
+// 메시지 이벤트 구독하기
+slackEvents.on('message', async (event) => {
+  
+  const message = event.text
+  console.log(message)
+  if(Number(event.text.split(' ')[0])){
+      webClient.chat.postMessage({
+          text : `사용금액 ~~원 기록되었습니다`,
+          channel : event.channel
+      });
+  } else {
+    return
+  }
+});
+
+// 메지지 이벤트 엔드포인트를 express 에 등록하기
+app.use('/slack/events', slackEvents.requestListener());
+
+// express 웹 서버 실행
+createServer(app).listen(3000, () => {
+  console.log('run slack bot');
+});
